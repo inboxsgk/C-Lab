3d053918949c30f82ab99f4f2cda96a08e833408
inboxsgk <30730201+inboxsgk@users.noreply.github.com>
2022-02-01 16:33:02 +0530

    Create Digits Reversal.c

---
+++ Digits Reversal.c
@@ -0,0 +1,22 @@
+//Write a c program to find the reverse of the digits of a given number using pointers only
+
+#include <stdio.h>
+int main()
+{ 
+  char number, *nptr;int l;
+  printf("Enter a number : ");
+  scanf("%s", &number);
+
+  nptr=&number;
+
+  for (int i=0; *(nptr+i)!='\0';i++){l=i;}
+
+  printf("\nThe reversed digits are : ");
+  nptr=&number;
+
+  for (int i=l; i>=0; i--){
+    printf("%c", *(nptr+i));
+  }
+  
+  return 0;
+}
